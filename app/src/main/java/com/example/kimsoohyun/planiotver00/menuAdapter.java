8577e3e7f19c8a9ce8436afebfff5ee246ea722e
Soo Hyun <shkim950921@naver.com>
2017-08-02 12:52:02 +0900

    주석지움

--- app/src/main/java/com/example/kimsoohyun/planiotver00/menuAdapter.java
+++ app/src/main/java/com/example/kimsoohyun/planiotver00/menuAdapter.java
@@ -1,10 +1,12 @@
 package com.example.kimsoohyun.planiotver00;
 
+import java.util.ArrayList;
+
 /**
  * Created by kimsoohyun on 2017-08-01.
  */
 public class menuAdapter {
-    //private ArrayList<menuItem> menuarrayList = new ArrayList<>();
+    private ArrayList<menuItem> menuarrayList = new ArrayList<>();
     public menuAdapter(){
 
     }
