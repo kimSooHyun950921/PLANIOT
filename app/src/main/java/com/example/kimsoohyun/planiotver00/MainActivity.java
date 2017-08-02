98177773bef5c010a1f544dd6d9b2f2acd207493
Soo Hyun <shkim950921@naver.com>
2017-08-03 02:00:40 +0900

    리스트뷰이미지및텍스트 크기조절

--- app/src/main/java/com/example/kimsoohyun/planiotver00/MainActivity.java
+++ app/src/main/java/com/example/kimsoohyun/planiotver00/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.kimsoohyun.planiotver00;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu);
+        ListView listview;
+        menuAdapter adapter = new menuAdapter();
+        listview = (ListView)findViewById(R.id.myplantList);
+        listview.setAdapter(adapter);
+        adapter.addItem(ContextCompat.getDrawable(this,R.drawable.images),"달심이","2014년 4월 3일");
+
+
+
     }
 }
