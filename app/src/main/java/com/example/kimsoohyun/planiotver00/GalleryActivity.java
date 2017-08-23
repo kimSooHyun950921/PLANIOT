e6e7a8b4982df6606159fed28cd1b8b0eac8d79b
Soo Hyun <shkim950921@naver.com>
2017-08-23 13:55:44 +0900

    식물사진을 위한 갤러리 액티비티 추가

--- app/src/main/java/com/example/kimsoohyun/planiotver00/GalleryActivity.java
+++ app/src/main/java/com/example/kimsoohyun/planiotver00/GalleryActivity.java
@@ -3,11 +3,11 @@ package com.example.kimsoohyun.planiotver00;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class FacilitySchemeActivity extends AppCompatActivity {
+public class GalleryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_facility_scheme);
+        setContentView(R.layout.activity_gallery);
     }
 }
